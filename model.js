ca69cc69ee53d66b30bc8cdb65eeba6f8e7ba0ef
danz1337z <danielz@getacademy.no>
2022-03-15 13:31:49 +0100

    v.0

---
+++ model.js
@@ -0,0 +1,50 @@
+const model = {
+    // tilstand app
+    app: {
+        currentpage: "Login",
+        currentuser: "Benjamin",
+        antallFavoritter: 0,
+        antallHidet: 0,
+    },
+    // inputs til hver side
+    inputs: {
+        // inputs til startside
+        startpage: {
+            login: "",
+            pwd: "",
+        },
+        // inputs favoritter
+        favoritter: [],
+        // inputs til uønsket
+        hide: [],
+        // inputs til random
+        random: [],
+        // bilde
+        bilde: [],
+
+    },
+
+    //data
+
+    users: [
+        { name: "Benjamin", pwd: "Benjamin" },
+    ],
+
+    categories: [
+        {
+
+        }
+    ],
+
+    movies: [
+        {}
+    ],
+
+    series: [
+        {}
+    ],
+
+
+
+
+};
